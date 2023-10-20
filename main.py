c3d9ded5511c11bd0f7299cac2e2215db1aab47e
PRANAV K DILEEP <91619846+pranavkdileep@users.noreply.github.com>
2023-10-20 12:17:45 +0000

    docker

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template, jsonify
+from requests_toolbelt import MultipartEncoder
 import requests
 import os
 import json
@@ -37,27 +38,6 @@ def upload_file():
                 return render_template('upload.html', cid=file_url)
     return render_template('upload.html')
 
-@app.route('/video', methods=['GET', 'POST'])
-def upload_video():
-    if request.method == 'POST':
-        video_file = request.files['file']
-        text_file = request.files['file1']
-        if video_file and text_file:
-            headers = {
-                "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweGMwQzI0MTY2NTM4ZDg3MjI0ZDQ1YjU5RjI4NzExNTk2RTQ5NjEzNzUiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2OTc3ODY4NzQ2NzIsIm5hbWUiOiJ3ZWIzIn0.0kkTUd5tRhI8Khi8TEkE4OXyGGVY1zfHQzDqNIJigaw",
-                "Content-Type": "multipart/form-data"
-            }
-            files = {
-                'video.mp4': (video_file.filename, video_file, video_file.content_type),
-            }
-            response = requests.post('https://api.web3.storage/upload', headers=headers, files=files)
-            if response.status_code == 200:
-                responcc = response.text
-                responcc = json.loads(responcc)
-                cid = responcc['cid']
-                file_url = f'https://{cid}.ipfs.dweb.link'
-                return render_template('uploadvd.html', cid=file_url)
-    return render_template('uploadvd.html')
 
 
 if __name__ == '__main__':
